5a5af0a46d280df8ac4f5388dcc0cfc261e1bd97
Lanre039 <rajilat1000@gmail.com>
2021-07-09 11:12:57 +0100

    refactor fetch hook

--- src/hooks/useItems.tsx
+++ src/hooks/useItems.tsx
@@ -1,31 +1,34 @@
 import { useEffect, useState } from "react";
 import { IItems } from "../types";
 
+const fetchData = async () => {
+  const baseUrl = process.env.REACT_APP_API_URL;
+
+  const result = await fetch(baseUrl, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  const data = await result.json();
+  return data.data.meals as IItems[];
+};
+
 export default function useItems() {
   const [items, setItems] = useState<IItems[]>([]);
   const [loading, setLoading] = useState<boolean>(false);
 
-  const fetchData = async () => {
-    const baseUrl = process.env.REACT_APP_API_URL;
-
+  const fetchItems = async () => {
     setLoading(true);
-
-    const result = await fetch(baseUrl, {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-
-    const data = await result.json();
-
-    setItems(data.data.meals);
+    const data = await fetchData();
+    setItems(data);
     setLoading(false);
   };
 
   useEffect(() => {
     if (!items.length) {
-      fetchData();
+      fetchItems();
     }
   }, [items]);
 
